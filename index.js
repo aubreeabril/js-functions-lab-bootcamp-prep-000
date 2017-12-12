e5a691424d7b21d96d97a148408f05def21a9b6c
Aubree Abril <aubree.abril@gmail.com>
2017-12-12 02:06:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+
+function happyHolidays() {
+  return "Happy Holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy Holidays, ${name}!`
+}
+
+function happyHolidayTo(name, holiday) {
+  return `Happy ${holiday} to ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
